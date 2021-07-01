a3685c5b539256b7db9cd66269d33f362ab48428
nupur4015 <62908979+nupur4015@users.noreply.github.com>
2021-07-01 21:13:07 +0530

    Update video.js

--- public/video.js
+++ public/video.js
@@ -1,4 +1,4 @@
-let socket = io.connect("http://localhost:3000");
+let socket = io.connect("https://lower-worms-70452.herokuapp.com");
 let divvidcall = document.getElementById("videocall");
 let joinButton = document.getElementById("join");
 let userVideo = document.getElementById("user");
